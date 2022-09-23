b93bb270cc309dac81e318b727591c8d4dca099f
EduuCarvalho <educarvalho.engcivil@gmail.com>
2022-09-23 16:55:07 -0300

    renderizando palavras e letras clicadas, falta mudar imagens da forca e input de chutar palavra

--- src/App.js
+++ src/App.js
@@ -3,83 +3,137 @@ import { useState } from "react";
 import palavras from "./palavras";
 
 import forca0 from "./assets/forca0.png";
-/* import forca1 from "./assets/forca1.png";
+import forca1 from "./assets/forca1.png";
 import forca2 from "./assets/forca2.png";
 import forca3 from "./assets/forca3.png";
 import forca4 from "./assets/forca4.png";
 import forca5 from "./assets/forca5.png";
-import forca6 from "./assets/forca6.png"; */
+import forca6 from "./assets/forca6.png";
 
 export default function App() {
-    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    const [wordRandom , setWordRandom] = useState("");
-    const [disableKeyboard , setKeyboard] = useState('letter-disable');
-
+  const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  
+  const [currentKeyboard, setKeyboard] = useState("letter-disable");
+  const [arrayNewWord, setArrayNewWord] = useState([]); //OK
+  const [arraySelectedLetter, setArraySelectedLetter] = useState([]);
+  const hidenWord = "_";
+  const [hanginImg,setHanginImg] = useState([forca0, forca1, forca2, forca3, forca4, forca5, forca6]);
+  const [incorrectLetter, setIncorretLetters] = useState (0);
 
   function HangmanGame() {
     return (
       <div className="hangman">
-        <img src={forca0} alt="forca"/>
+        <img src={hanginImg[incorrectLetter]} alt="forca" />
         <div className="choose-word" onClick={RandomWord}>
           <strong>Escolher Palavra</strong>
         </div>
         <div className="word">
-          <p>{wordRandom}</p>
+          {arrayNewWord.map((l, index) =>
+            arraySelectedLetter.includes(l) ? (
+              <p key={index}>{l}</p>
+            ) : (
+              <p key={index}> {hidenWord} </p>
+            )
+          )}
         </div>
       </div>
     );
   }
 
-  function RandomWord (){
-    const indexRandom = Math.floor(Math.random()*palavras.length)
-    console.log(indexRandom);
+  function RandomWord() {
+    reset();
+    setKeyboard("letter-enable");
+    const indexRandom = Math.floor(Math.random() * palavras.length);
     const newWord = palavras[indexRandom];
-    const arrayNewWord = newWord.split('');
-    const hidenWord = arrayNewWord.map((l)=> "_ ")
-    const enableKeyboard = "letter-enable";
-    console.log(hidenWord);
-    console.log(newWord);
-    setWordRandom(hidenWord);
-    setKeyboard(enableKeyboard)
-  };
-
-/*   function Hanged() {
-    const hangin = [forca0, forca1, forca2, forca3, forca4, forca5, forca6];
-  } */
-  
-  
-
-
-  function Keyboard(props) {
-    
-
-    return (
-        <div className={disableKeyboard}> 
-            {props.letter.toUpperCase()}
-        </div>
-    )
+    setArrayNewWord(newWord.split(""));
+    console.log ("indice da palavra criada",indexRandom );
+    console.log ("palavra criada",newWord );
     
   }
 
-  function TryToAnswer (){
-    return (
-        <div className="answer">
-            <p>Já sei a palavra!</p>
-            <input></input>
-            <button>Chutar</button>
-        </div>
+  function reset () {
+    setKeyboard("letter-disable");
+    setArrayNewWord ([]);
+    setArraySelectedLetter([]); 
 
+  };
+
+/* function loseOrWin () {
+
+  incorrectLetter.map((l,index)=> arrayNewWord.includes(l)?  : (setHanginImg(+1)));
+
+}; */
+
+  
+  
+
+  function Keyboard(props) {
+    return (
+      <div
+        className={
+          arraySelectedLetter.includes(props.letter)
+            ? "letter-disable"
+            : currentKeyboard
+        }
+        onClick={() => chooseLetter(props.index)}
+      >
+        {props.letter.toUpperCase()}
+      </div>
     );
   }
 
+  function chooseLetter(s) {
+    const selectedLetter = [...arraySelectedLetter, alphabet[s]];
+    setArraySelectedLetter(selectedLetter); 
+    console.log("array letras clicadas", arraySelectedLetter);
+  }
+
+  function TryToAnswer() {
+    return (
+      <div className="answer">
+        <p>Já sei a palavra!</p>
+        <input></input>
+        <button>Chutar</button>
+      </div>
+    );
+  }
 
   return (
     <div className="game-page">
       <HangmanGame />
       <div className="keyboard">
-      {alphabet.map((l,index)=> (<Keyboard letter={l} key={index}/>))}
+        {alphabet.map((l, index) => (
+          <Keyboard letter={l} key={index} index={index} />
+        ))}
       </div>
-      <TryToAnswer/>
+      <TryToAnswer />
     </div>
   );
 }
